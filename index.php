ad922c3300c302e4185f3c91ba774f2252e7dbd9
abn <ikomangbramantara@gmail.com>
2026-01-02 19:56:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$text = "METHOD CANT READ BY SERVER, ERR:404"
+  ?>
+<!DOCTYPE html>
+<head>
+  <title>Bot API by .lb (Bramantara)</title>
+</head>
+<body>
+  <h1><?php echo $text ?></h1>
+</body>
