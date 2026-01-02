85715e14e653cf71116137d2b86efadeb82a46f1
abn <ikomangbramantara@gmail.com>
2026-01-02 19:38:09 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// TikTok Service IDs dari Zefame
+define('TIKTOK_VIEWS_SERVICE_ID', 229);
+define('TIKTOK_LIKES_SERVICE_ID', 232);
+define('ZEFAME_API_URL', 'https://zefame-free.com/api_free.php');
+
+// Rate limiting
+define('MAX_REQUESTS_PER_MINUTE', 30);
+?>
